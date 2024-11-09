eb9cffd7a5f63f98fc12fab5d2f3acc35837cc71
Prabhakar <prabhakarkumar313@gmail.com>
2024-11-09 10:32:44 +0530

    logger and exception added

---
+++ sensor/exception.py
@@ -0,0 +1,27 @@
+import os
+import sys
+
+
+
+
+def error_message_detail(error,error_detail:sys):
+
+    _,_,exc_tb = error_detail.exc_info()
+    filename = exc_tb.tb_frame.f_code.co_filename
+
+    error_message = f"Error occurred in {filename} at line number [{exc_tb.tb_lineno}] Error is: [{error}]"
+
+    return error_message
+
+
+class SensorException(Exception):
+
+    def __init__(self,error_message,error_detail:sys):
+
+        super().__init__(error_message)
+
+        self.error_message = error_message_detail(error_message,error_detail=error_detail)
+
+
+    def __str__(self):
+        return self.error_message
