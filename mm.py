0ead3330e3a5718fdcd99fde96f5bb38e8af616f
Prabhakar <prabhakarkumar313@gmail.com>
2024-11-10 20:56:23 +0530

    working on ingestion

--- mm.py
+++
@@ -1,8 +0,0 @@
-import pymongo
-from pymongo import MongoClient
-
-connection_string = "mongodb+srv://prabhakarkumar313:9571002370@cluster0.kztw6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-client = MongoClient(connection_string)
-print("hel")
-print(client.list_database_names())
-db = client["machinelearningdb"]
