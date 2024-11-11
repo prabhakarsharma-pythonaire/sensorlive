617a837a3c2e86e9fe392381bec3846258859b7c
Prabhakar <prabhakarkumar313@gmail.com>
2024-11-12 00:36:29 +0530

    till transformation everything working fine

--- sensor/components/data_ingestion.py
+++ sensor/components/data_ingestion.py
@@ -1,17 +1,18 @@
-
+import os
+import sys
 from sensor.exception import SensorException
 from sensor.logger import logging
-import os 
-import sys
+
 from pandas import DataFrame
 from sensor.entity.config_entity import DataIngestionConfig
 from sensor.entity.artifact_entity import DataIngestionArtifact
 from sensor.data_access.sensor_data import SensorData
-from sklearn.model_selection import train_test_split
+
 
 from sensor.utils.main_utils import read_yaml_file
 from sensor.constant.training_pipeline import SCHEMA_FILE_PATH
 
+from sklearn.model_selection import train_test_split
 
 class DataIngestion:
     def __init__(self,data_ingestion_config:DataIngestionConfig):
